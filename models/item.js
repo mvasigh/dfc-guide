d6d4a62d8deee19ed67274da86b31553087c064c
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-04-15 16:11:30 -0500

    Added categories and content

--- models/item.js
+++ models/item.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
 const itemSchema = new mongoose.Schema({
-    title: String,
-    description: String
+  title: String,
+  description: String,
+  categories: [String],
+  content: String
 });
 
-module.exports = mongoose.model("Item", itemSchema);
+module.exports = mongoose.model('Item', itemSchema);
