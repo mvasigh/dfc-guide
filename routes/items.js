38ae7ed0df37ce189255ad93f2c0c52b142b367b
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-04-18 14:06:08 -0500

    Created item INDEX view

--- routes/items.js
+++ routes/items.js
@@ -2,19 +2,31 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 const Item = require('../models/item');
 
+// INDEX - show all items
+router.get('/', (req, res) => {
+  Item.find({}, (err, items) => {
+    if (err) {
+      console.log(err);
+      res.redirect('back');
+    } else {
+      res.render('item/index', { items: items });
+    }
+  });
+});
+
 // NEW - show new item page
 router.get('/new', (req, res) => {
   res.render('item/new');
 });
 
+// CREATE - create a new item in db
 router.post('/', (req, res) => {
   console.log(req.body);
   Item.create(req.body.item, (err, item) => {
     if (err) {
       console.log(err);
     } else {
-      console.log('Created a new item', item);
-      res.render('home');
+      res.redirect(`/item/${item._id}`);
     }
   });
 });
