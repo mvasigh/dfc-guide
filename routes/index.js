0a855287e8909847374f7eee82ae21416f3b91d2
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-05-30 22:11:35 -0500

    update to use async/await

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,15 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
+
 const Item = require('../models/item');
 
 router.get('/', (req, res) => {
   res.redirect('/home');
 });
 
-router.get('/home', (req, res) => {
-  Item.find({}, (err, items) => {
-    if (err) {
-      res.render('home', {
-        items: []
-      });
-    } else {
-      res.render('home', {
-        items: items
-      });
-    }
-  });
+router.get('/home', async (req, res) => {
+  const items = await Item.find({});
+  res.render('home', { items });
 });
 
 module.exports = router;
