d0213f3cd1f6fae7e80b92f6d8ac714d1a389d0f
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-06-02 10:00:46 -0500

    reorganize app.js and add PORT environment variable

--- app.js
+++ app.js
@@ -6,24 +6,25 @@ const expressSanitizer = require('express-sanitizer');
 const helmet = require('helmet');
 const seedDB = require('./seeds');
 const methodOverride = require('method-override');
+
 const itemsRoutes = require('./routes/items');
 const categoriesRoutes = require('./routes/categories');
 const guidesRoutes = require('./routes/guides');
 const topicsRoutes = require('./routes/topics');
 const indexRoutes = require('./routes/index');
 
-mongoose.connect('mongodb://localhost/dfc_guide');
 
 // ==============
 // APP CONFIG
 // ==============
+mongoose.connect('mongodb://localhost/dfc_guide');
 const app = express();
 
 app.engine('ejs', engine);
 app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
-app.set('view engine', 'ejs');
 app.use(
   bodyParser.urlencoded({
     extended: true
@@ -44,6 +45,5 @@ app.use('/guides', guidesRoutes);
 app.use('/topics', topicsRoutes);
 app.use('/', indexRoutes);
 
-app.listen(3000, () => {
-  console.log('Server is running...');
-});
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
