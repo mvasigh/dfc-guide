a85e90c4f62ae6dbb36d4c21d119b3fda57ca8c7
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-06-14 17:18:20 -0500

    adjust fuse.js config values and weight search keys

--- config/fuse_config.js
+++ config/fuse_config.js
@@ -1,8 +1,23 @@
 module.exports = {
-  keys: ['title', 'tags', 'category.name', 'description'],
+  keys: [
+    {
+      name: 'title',
+      weight: 0.6
+    },
+    {
+      name: 'tags',
+      weight: 0.2
+    },
+    {
+      name: 'category.name',
+      weight: 0.2
+    }
+  ],
   minMatchCharLength: 1,
   shouldSort: true,
   findAllMatches: true,
   matchAllTokens: false,
-  tokenize: true
+  tokenize: true,
+  threshold: 0.6,
+  location: 10
 };
