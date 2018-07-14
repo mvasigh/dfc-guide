3c5c4c9e8079886e330fa825dc4c9de71c03ccac
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-14 17:15:41 -0500

    add action types and fetchItems action creator

---
+++ client/src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export const FETCH_ITEMS = 'fetch_items';
+export const FETCH_CATEGORIES = 'fetch_categories';
+export const FETCH_GUIDES = 'fetch_guides';
+export const FETCH_TOPICS = 'fetch_topics';
+
+export function fetchItems() {
+  const request = axios.get('/api/items');
+
+  return {
+    type: FETCH_ITEMS,
+    payload: request
+  };
+}
