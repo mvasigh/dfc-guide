10147d74248bf7f4a30b5a97f06acd9c9402c798
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-14 17:17:07 -0500

    wire page to Redux and render most viewed items

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
-import { ItemsList } from '../components/ItemsList';
-import ItemCard from '../components/ItemCard';
+import _ from 'lodash';
+import { connect } from 'react-redux';
+import { fetchItems } from '../actions';
 
-export class Home extends Component {
+import ItemCard from '../components/ItemCard';
+import logo from '../logo.svg';
+
+class Home extends Component {
+  componentDidMount() {
+    this.props.fetchItems();
+  }
+
+  renderMostViewed() {
+    if (this.props.items != {}) {
+      const items = _.sortBy(this.props.items, 'views').slice(0, 6);
+      return _.map(items, (item, i) => (
+        <div key={i} className="column is-one-third">
+          <ItemCard
+            title={item.title}
+            description={item.description}
+            category={item.category.name}
+            id={item._id}
+            tags={item.tags}
+          />
+        </div>
+      ));
+    }
+  }
+
   render() {
     return (
       <div>
@@ -40,16 +64,28 @@ export class Home extends Component {
             </div>
           </div>
         </section>
-        <ItemCard
-          id={1}
-          title="Hello World"
-          description="Wow this is truly a wonderful React component"
-          category="test"
-          tags={['hello', 'world']}
-        />
+        <section className="section">
+          <div className="container">
+            <p className="title is-4">Getting Started</p>
+            <p className="title is-4">Most Viewed</p>
+            <p className="subtitle has-text-grey is-6">
+              View our most frequently accessed resources.
+            </p>
+            <div className="columns is-multiline">
+              {this.renderMostViewed()}
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
 }
 
-export default Home;
+function mapStateToProps(state) {
+  return { items: state.items };
+}
+
+export default connect(
+  mapStateToProps,
+  { fetchItems }
+)(Home);
