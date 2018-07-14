624f8ea7bf4135c84cefe3234f88d2c415a2f87d
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-14 17:15:18 -0500

    add default values for ItemCard props

--- client/src/components/ItemCard.js
+++ client/src/components/ItemCard.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const ItemCard = ({ id, title, category, tags, description }) => {
+const ItemCard = ({
+  id,
+  title,
+  category = '',
+  tags = [],
+  description = ''
+}) => {
   const renderTags = tags =>
     tags.map((tag, i) => (
-      <Link className="tag is-light is-rounded" to={`/items?search=${tag}`}>
+      <Link
+        key={i}
+        className="tag is-light is-rounded"
+        to={`/items?search=${tag}`}
+      >
         {tag}
       </Link>
     ));
