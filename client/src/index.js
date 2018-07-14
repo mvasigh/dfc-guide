84407ab90abeba1d01c77329b982c650ab59a118
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-13 21:40:07 -0500

    remove unnecessary CSS

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import promise from 'redux-promise';
 import { createStore, applyMiddleware } from 'redux';
+import './index.css';
 
 import reducers from './reducers';
 import App from './App';
