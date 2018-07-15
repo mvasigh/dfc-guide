b9bf981c47437b1c196b873cea9fe697fc859f8f
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-15 14:49:05 -0500

    add fetchGuides, fetchTopics and fetchItem action creators

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_ITEMS = 'fetch_items';
+export const FETCH_ITEM = 'fetch_item';
 export const FETCH_CATEGORIES = 'fetch_categories';
 export const FETCH_GUIDES = 'fetch_guides';
 export const FETCH_TOPICS = 'fetch_topics';
@@ -14,11 +15,38 @@ export function fetchItems() {
   };
 }
 
+export function fetchItem(itemId) {
+  const request = axios.get(`/api/items/${itemId}`);
+
+  return {
+    type: FETCH_ITEM,
+    payload: request
+  };
+}
+
 export function fetchCategories() {
   const request = axios.get('/api/categories');
 
   return {
     type: FETCH_CATEGORIES,
+    payload: request
+  };
+}
+
+export function fetchGuides() {
+  const request = axios.get('/api/guides');
+
+  return {
+    type: FETCH_GUIDES,
+    payload: request
+  };
+}
+
+export function fetchTopics() {
+  const request = axios.get('/api/topics');
+
+  return {
+    type: FETCH_TOPICS,
     payload: request
   };
 }
