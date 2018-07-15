8bcf72bbdbbd8ed7143d158f8543ae5215a65dcb
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-15 01:40:04 -0500

    add renderGetStarted helper method

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,10 +5,43 @@ import { fetchItems } from '../actions';
 
 import ItemCard from '../components/ItemCard';
 import logo from '../logo.svg';
+import { Link } from 'react-router-dom';
+import BasicCard from '../components/BasicCard';
 
 class Home extends Component {
   componentDidMount() {
     this.props.fetchItems();
+  }
+
+  renderGetStarted() {
+    const getStartedLinks = [
+      {
+        title: 'Browse Community Resources',
+        description:
+          "Search our categorized database of community resources for your patients' unmet needs",
+        to: '/items'
+      },
+      {
+        title: 'Access our Guide Book',
+        description:
+          'Read helpful information about screening for unmet needs, insurance options, eligibility, and more.',
+        to: '/guides'
+      },
+      {
+        title: 'How to Use the DFC Guide',
+        description:
+          'Learn how you can most effectively use the Guide to find what you are looking for.',
+        to: '/guides'
+      }
+    ];
+
+    return getStartedLinks.map((link, i) => (
+      <div key={i} className="column is-one-third">
+        <Link to={link.to}>
+          <BasicCard title={link.title} description={link.description} />
+        </Link>
+      </div>
+    ));
   }
 
   renderMostViewed() {
@@ -67,6 +100,9 @@ class Home extends Component {
         <section className="section">
           <div className="container">
             <p className="title is-4">Getting Started</p>
+            <div className="columns is-multiline">
+              {this.renderGetStarted()}
+            </div>
             <p className="title is-4">Most Viewed</p>
             <p className="subtitle has-text-grey is-6">
               View our most frequently accessed resources.
