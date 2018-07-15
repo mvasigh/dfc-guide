7e076cab25825e898f09ce3c75b2be57fd6c1812
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-15 01:40:53 -0500

    add categoriesReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import itemsReducer from './reducer_items';
+import categoriesReducer from './reducer_categories';
 
 const rootReducer = combineReducers({
-  items: itemsReducer
+  items: itemsReducer,
+  categories: categoriesReducer
 });
 
 export default rootReducer;
