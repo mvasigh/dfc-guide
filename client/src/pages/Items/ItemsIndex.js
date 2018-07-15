100d39eeb996c77ba6ceb363247e82aebb0983bd
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-15 16:28:09 -0500

    add category filtering functionality

--- client/src/pages/Items/ItemsIndex.js
+++ client/src/pages/Items/ItemsIndex.js
@@ -7,12 +7,41 @@ import CategoryMenu from '../../components/CategoryMenu';
 import ItemsList from '../../components/ItemsList';
 
 class ItemsIndex extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      filterCategoryName: ''
+    };
+
+    this.handleCategoryClick = this.handleCategoryClick.bind(this);
+  }
+
   componentDidMount() {
     this.props.fetchItems();
     this.props.fetchCategories();
+
+    if (this.props.location.state) {
+      this.setState({ filterCategoryName: this.props.location.state.filter });
+    }
   }
 
-  renderItemList() {}
+  renderItemList() {
+    if (this.state.filterCategoryName === '') {
+      return <ItemsList items={this.props.items} />;
+    } else {
+      const filteredItems = _.filter(
+        this.props.items,
+        item => item.category.name === this.state.filterCategoryName
+      );
+      return <ItemsList items={filteredItems} />;
+    }
+  }
+
+  handleCategoryClick(e) {
+    const filterCategoryName = e.target.id;
+    this.setState({ filterCategoryName });
+  }
 
   render() {
     return (
@@ -21,10 +50,13 @@ class ItemsIndex extends Component {
           <div className="container">
             <div className="columns">
               <div className="column is-one-third">
-                <CategoryMenu />
+                <CategoryMenu
+                  active={this.state.filterCategoryName}
+                  onCategoryClick={this.handleCategoryClick}
+                />
               </div>
               <div className="column is-two-thirds">
-                <ItemsList items={this.props.items} />
+                {this.renderItemList()}
               </div>
             </div>
           </div>
