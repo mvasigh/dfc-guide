63590e640f4e3203209170547aa07fcdb57a4b49
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-15 17:13:12 -0500

    add hamburger menu and onClick behavior

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,39 +1,81 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => {
-  return (
-    <nav
-      className="navbar is-fixed-top is-shadowless is-info"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <Link className="navbar-item dfc-logo-container" to="/home">
-            DFC
-          </Link>
-        </div>
-        <div className="navbar-menu">
-          <div className="navbar-start" />
-          <div className="navbar-end">
-            <Link className="navbar-item" to="/items">
-              Community Resources
-            </Link>
-            <Link className="navbar-item" to="/guides">
-              Guide Book
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false
+    };
+
+    this.handleMenuClick = this.handleMenuClick.bind(this);
+    this.handleMenuItemClick = this.handleMenuItemClick.bind(this);
+  }
+
+  handleMenuClick() {
+    const expanded = !this.state.expanded;
+    this.setState({ expanded });
+  }
+
+  handleMenuItemClick() {
+    this.setState({ expanded: false });
+  }
+
+  render() {
+    return (
+      <nav
+        className="navbar is-fixed-top is-shadowless is-info"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div className="container">
+          <div className="navbar-brand">
+            <Link className="navbar-item dfc-logo-container" to="/home">
+              DFC
             </Link>
             <a
-              className="navbar-item"
-              href="https://docs.google.com/forms/d/1V5ySudx03DRinmNHCm6EfKWXnPa1GJAyVxaZcO5Miy0/viewform"
+              role="button"
+              className="navbar-burger has-text-white"
+              aria-label="menu"
+              aria-expanded="false"
+              onClick={this.handleMenuClick}
             >
-              Feedback
+              <span aria-hidden="true" />
+              <span aria-hidden="true" />
+              <span aria-hidden="true" />
             </a>
           </div>
+          <div
+            className={`navbar-menu ${this.state.expanded ? 'is-active' : ''}`}
+          >
+            <div className="navbar-start" />
+            <div className="navbar-end">
+              <Link
+                onClick={this.handleMenuItemClick}
+                className="navbar-item"
+                to="/items"
+              >
+                Community Resources
+              </Link>
+              <Link
+                onClick={this.handleMenuItemClick}
+                className="navbar-item"
+                to="/guides"
+              >
+                Guide Book
+              </Link>
+              <a
+                className="navbar-item"
+                href="https://docs.google.com/forms/d/1V5ySudx03DRinmNHCm6EfKWXnPa1GJAyVxaZcO5Miy0/viewform"
+              >
+                Feedback
+              </a>
+            </div>
+          </div>
         </div>
-      </div>
-    </nav>
-  );
-};
+      </nav>
+    );
+  }
+}
 
 export default Navbar;
