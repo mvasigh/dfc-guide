38e406c074ce4766cb406e7a35f9a8ea13d450d4
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-17 23:45:10 -0500

    refactor routes and add ScrollToTop

--- client/src/App.js
+++ client/src/App.js
@@ -2,36 +2,43 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
-
 import Home from './pages/Home';
 import ItemsIndex from './pages/Items/ItemsIndex';
 import ItemsShow from './pages/Items/ItemsShow';
 import Guides from './pages/Guides';
-import Terms from './pages/Terms';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div>
+        <ScrollToTop>
           <Navbar />
           <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/home" component={Home} />
-            <Route path="/terms" component={Terms} />
+            <Route
+              exact
+              path="/"
+              render={props => <Home key={props.location.href} {...props} />}
+            />
             <Route
               path="/items/:itemId"
               render={props => (
                 <ItemsShow key={props.match.params.itemId} {...props} />
               )}
             />
-            <Route path="/items" component={ItemsIndex} />
+            <Route
+              exact
+              path="/items"
+              render={props => (
+                <ItemsIndex key={props.location.search} {...props} />
+              )}
+            />
             <Route path="/guides/:guideId" component={Guides} />
             <Route exact path="/guides" component={Guides} />
           </Switch>
           <Footer />
-        </div>
+        </ScrollToTop>
       </BrowserRouter>
     );
   }
